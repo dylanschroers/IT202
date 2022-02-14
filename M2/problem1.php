017a64b92f7230361c9a9b2673a8f7161efc606c
dylanschroers <djs28@njit.edu>
2022-02-13 23:48:22 -0500

    completed hw

--- M2/problem1.php
+++ M2/problem1.php
@@ -8,7 +8,11 @@ function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //TODO add logic here to echo out only odd values
-
+    foreach($arr as $i) {
+        if ((int)$i%2 == 1) {
+            echo " " . (int)$i;    
+        }
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
