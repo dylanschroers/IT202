7a4ba9ad558673ce228d8b58feae9f9185e95e00
dylanschroers <djs28@njit.edu>
2022-03-11 01:08:31 -0500

    login nav part 2

--- public_html/Project/logout.php
+++ public_html/Project/logout.php
@@ -2,8 +2,4 @@
 session_start();
 session_unset();
 session_destroy();
-//setcookie("PHPSESSID", "", time()-3600);
-session_start();
-require_once(__DIR__ . "/../../lib/functions.php");
-flash("You have been logged out", "success");
-die(header("Location: login.php"));
+header("Location: login.php");
