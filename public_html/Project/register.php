b4b82feab830449a3d31354e7e3af68e40a18147
dylanschroers <djs28@njit.edu>
2022-04-04 22:50:23 -0400

    new register

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -3,6 +3,27 @@ require(__DIR__ . "/../../partials/nav.php");
 session_unset();
 session_destroy();
 session_start();
+function users_check_duplicate($errorInfo)
+{
+    if ($errorInfo[1] === 1062) {
+        //https://www.php.net/manual/en/function.preg-match.php
+        //NOTE: this assumes your table name is `Users`, edit it accordingly
+        preg_match("/Users.(\w+)/", $errorInfo[2], $matches);
+        if (isset($matches[1])) {
+            flash("The chosen " . $matches[1] . " is not available.", "warning");
+        } else {
+            //TODO come up with a nice error message
+            flash("An unhandled error occurs", "danger");
+            //this will log the output to the terminal/console that's running the php server
+            error_log(var_export($errorInfo, true));
+        }
+    } else {
+        //TODO come up with a nice error message
+        flash("An unhandled error occurs", "danger");
+        //this will log the output to the terminal/console that's running the php server
+        error_log(var_export($errorInfo, true));
+    }
+}
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
@@ -30,6 +51,7 @@ session_start();
 
         return true;
     }
+    
 </script>
 <?php
 //TODO 2: add PHP Code
