6e932445849baa87caffd62ffaaa4ce36470f0c3
Dylan Schroers <dylanschroers@Dylans-MacBook-Air.local>
2022-05-11 20:22:41 -0400

    completed loans

--- public_html/Project/dashboard.php
+++ public_html/Project/dashboard.php
@@ -21,5 +21,8 @@ require(__DIR__ . "/../../partials/flash.php");
     <li><a href="deposit.php">Deposits</a></li>
     <li><a href="withdraw.php">Withdraw</a></li>
     <li><a href="transfers.php">Transfer</a></li>
+    <li><a href="loans.php">Loans</a></li>
+    <li><a href="payLoan.php">Pay Loan</a></li>
+    <li><a href="close_accounts.php">Close Accounts</a></li>
     <li><a href="profile.php">Profile</a></li>
 <?php endif; ?>
--- public_html/Project/loans.php
+++ public_html/Project/loans.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
 ?>
-<li><a href="ext_transfers.php">External Transfer</a></li>
+
 <?php
 $db = getDB();
 
@@ -17,48 +17,60 @@ try {
 }
 
 if (isset($_POST["save"])) {
-    $srcAcc = se($_POST, "srcAcc", null, false);
     $destAcc = se($_POST, "destAcc", null, false);
-    $transfer = se($_POST, "transfer", null, false);
+    $loan = se($_POST, "loan", null, false);
     $memo = se($_POST, "memo", null, false);
+    $apy = 0.125;
 
     try {
         $query = "SELECT balance from Accounts WHERE id = :aid";
         $stmt = $db->prepare($query);
-        $stmt->execute(["aid" => $srcAcc]);
+        $stmt->execute(["aid" => $destAcc]);
         $account = $stmt->fetch(PDO::FETCH_ASSOC);
         $b = (int)se($account, "balance", 0, false);
 
-        if ($b < $transfer) {
+        if ($b < $loan) {
             flash("Cannot transfer more than the account holds");
         }
-        else if ($srcAcc == $destAcc) {}
         else {
             $id = $db->lastInsertId();
-            //this should mimic what's happening in the DB without requiring me to fetch the data
-            
+            //creates the loan account
+            $query = "INSERT INTO Accounts (user_id, account_type, account_number, balance) VALUES (:uid, :accType, NULL, :bal)";
+            $user_id = get_user_id(); //caching a reference
+            $stmt = $db->prepare($query);
+            $stmt->execute([":uid" => $user_id, ":accType" => "Loan", ":bal" => $loan]);
+            $id = $db->lastInsertId();
 
-            //src account
+            $account_number = str_pad($id, 12, "0", STR_PAD_LEFT);
+            $query = "UPDATE Accounts SET account_number = :account_number where id = :id";
+            $stmt = $db->prepare($query);
+            $stmt->execute([":id" => $id, ":account_number" => $account_number]);
+
+            $query = "INSERT INTO SysProp (account_number, apy) VALUES (:accNum, :apy)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(["accNum" => $account_number, ":apy" => $apy]);
+
+            //loan account
             $query = "INSERT INTO Transactions (account_src, account_dest, balance_change, transaction_type, memo, expected_total) 
             VALUES (:accSrc, :accDest, :balC, :tranType, :mem, NULL)";
             $stmt = $db->prepare($query);
-            $stmt->execute([":accSrc" => $srcAcc, ":accDest" => $destAcc, ":balC" => -1*$transfer, ":tranType" => "Internal Transfer", ":mem" => $memo]);
+            $stmt->execute([":accSrc" => $account_number, ":accDest" => $destAcc, ":balC" => $loan*($apy+1), ":tranType" => "Loan", ":mem" => $memo]);
             $id = $db->lastInsertId();
 
             $query = "UPDATE Accounts SET balance = (SELECT IFNULL(SUM(balance_change), 0) 
             from Transactions WHERE account_src = :src) where id = :src";
             $stmt = $db->prepare($query);
-            $stmt->execute([":src" => $srcAcc]);
+            $stmt->execute([":src" => $account_number]);
 
             $query = "UPDATE Transactions SET expected_total = (SELECT IFNULL(SUM(balance), 0) from Accounts WHERE id = :aid) where id = :id";
             $stmt = $db->prepare($query);
-            $stmt->execute(["aid" => $srcAcc, "id" => $id]);
+            $stmt->execute(["aid" => $account_number, "id" => $id]);
             
             //dest account
             $query = "INSERT INTO Transactions (account_src, account_dest, balance_change, transaction_type, memo, expected_total) 
             VALUES (:accSrc, :accDest, :balC, :tranType, :mem, :exTot)";
             $stmt = $db->prepare($query);
-            $stmt->execute([":accSrc" => $destAcc, ":accDest" => $srcAcc, ":balC" => $transfer, ":tranType" => "Internal Transfer", ":mem" => $memo, ":exTot" => $transfer]);
+            $stmt->execute([":accSrc" => $destAcc, ":accDest" => $account_number, ":balC" => $loan, ":tranType" => "Loan", ":mem" => $memo, ":exTot" => $loan]);
             $id = $db->lastInsertId();
 
             $query = "UPDATE Accounts SET balance = (SELECT IFNULL(SUM(balance_change), 0) 
@@ -89,9 +101,8 @@ if (isset($_POST["save"])) {
 
 <script>
     function validate(form) {
-        let sAcc = form.srcAcc.value;
         let dAcc = form.destAcc.value;
-        let dep = form.transfer.value;
+        let loan = form.loan.value;
         let isValid = true;
         //TODO add other client side validation....
 
@@ -101,13 +112,9 @@ if (isset($_POST["save"])) {
             flash("Accounts must be different", "warning");
             isValid = false;
         }
-        if (!(sAcc)) {
-            flash("Account must not be empty", "warning");
-            isValid = false;
-        }
 
-        if (!(dep > 1)) {
-            flash("Transfer must be greater than 1", "warning");
+        if (!(loan > 500)) {
+            flash("Loan must be greater than 1", "warning");
             isValid = false;
         }
         return isValid;
@@ -115,18 +122,6 @@ if (isset($_POST["save"])) {
 </script>
 
 <form method="POST" onsubmit="return validate(this);">
-    <div class="mb-3">
-        <label for="srcAcc" class="form-label">Source Account</label>
-        <select id="srcAcc" name="srcAcc" class="form-control">
-            <?php foreach ($results as $al) : ?>
-                <option value="<?php se($al, 'id'); ?>"><?php se($al, 'account_number'); ?></option>
-            <?php endforeach; ?>
-        </select>
-    </div>
-    <div class="mb-3">
-        <label for="transfer">Transfer</label>
-        <input type="number" name="transfer" id="transfer" value="<?php se("5"); ?>" />
-    </div>
     <div class="mb-3">
         <label for="destAcc" class="form-label">Destination Account</label>
         <select id="destAcc" name="destAcc" class="form-control">
@@ -136,10 +131,14 @@ if (isset($_POST["save"])) {
         </select>
     </div>
     <div class="mb-3">
+        <label for="loan">Loan Amount</label>
+        <input type="number" name="loan" id="loan" value="<?php se("500"); ?>" />
+    </div>
+    <div class="mb-3">
         <label for="memo">Memo</label>
         <input type="text" name="memo" id="memo" />
     </div>
-    <input type="submit" value="Transfer" name="save" />
+    <input type="submit" value="Take Loan" name="save" />
 </form>
 <?php
 require_once(__DIR__ . "/../../partials/flash.php");
